f0a7409f5b66e2be6042b29147c177fe9149a600
larhra <agbajeaminah@yahoo.com>
2025-02-16 20:04:00 +0100

    first react project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,16 +6,20 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
+  ["name=", "Alice", "age=", "25", "bio=", "Loves hiking and photography"]
 
   return (
     <>
       <WelcomeMessage />  
       <Footer />
       <Header />
-      <MainContent /> 
+      <MainContent />
+      <UserProfile /> 
+
     </>
   )
 }
