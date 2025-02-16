1973e8d56fd658dd1d8ec5f0278358c54ea5703e
larhra <agbajeaminah@yahoo.com>
2025-02-16 19:49:04 +0100

    first react project

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
 
 function App() {
   const [count, setCount] = useState(0)
