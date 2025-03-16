d6bfa5df4a8c3f7865f2c4028cd030d1afef2017
larhra <agbajeaminah@yahoo.com>
2025-03-16 19:43:34 +0100

    react post

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-green-500 text-white text-3xl font-bold">
+    <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white text-3xl font-bold">
       Tailwind CSS is Working! ✅
     </div>
   );
