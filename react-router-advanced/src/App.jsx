7828550aad08ddaadc22e9620ed22af3f09222ba
larhra <agbajeaminah@yahoo.com>
2025-03-22 06:46:37 +0100

    react post

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Profile from './components/Profile';
+import Post from './components/Post';
 import BlogPost from './components/BlogPost';
-import Login from './components/Login';
-import './App.css'
 import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
-
+const App = () => {
   return (
     <Router>
-      <nav>
-        <Link to="/profile">Profile</Link>
-      </nav>
-      <Routes>
-        <Route path='/profile/*' element={<ProtectedRoute component={<Profile />} />} />
-        <Route path='/blog/:id' element={<BlogPost />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<h2>Home</h2>} />
-      </Routes>
+      <div>
+        <h1>React Router Advanced Demo</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="profile/*" element={<Profile />} /> {/* Profile route is now the parent route */}
+          <Route path="post/:id" element={<Post />} />
+          <Route path="/blog/:id" element={<BlogPost  />} />
+          <Route path="/profile" element={<ProtectedRoute  />} />
+        </Routes>
+      </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
