9a7a39f563e66c8a0da669d49d2c6f503c0f1a7e
larhra <agbajeaminah@yahoo.com>
2025-03-27 09:14:12 +0100

    Enhancing User Interface

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,21 @@
-
+import src from "../src/assets/react.svg";
 function UserProfile() {
-    return (
-      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm sm:w-24 md:w-36 hover:shadow-xl">
-        <img src="https://www.alxafrica.com/wp-content/uploads/2023/12/logo-black.svg" alt="User" className="rounded-full w-36 h-36 mx-auto sm:h-24 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"/>
-        <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
-        <p className="text-gray-600 text-base sm:text-sm md:text-base hover:text-blue-500">Developer at Example Co. Loves to write code and explore new technologies.</p>
-      </div>
-    );
-  }
-  
-  export default UserProfile;
+  return (
+    <div className=" hover:shadow-xl transition-shadow duration-300 ease-in-out bg-gray-100 sm:p-4 sm:p-6 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 sm:my-16 md:my-20 rounded-lg shadow-lg text-center sm:w-24 sm:h-24">
+      <img
+        src={src}
+        alt="User"
+        className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+      />
+      <h1 className=" hover:text-blue-500 transition-colors duration-300 ease-in-out text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-base text-gray-600 md:text-xl">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
