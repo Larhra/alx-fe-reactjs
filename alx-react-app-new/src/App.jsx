7b2883150640ee7c75fdacd62c74300920a2a9d4
larhra <agbajeaminah@yahoo.com>
2025-04-07 13:12:14 +0100

    app new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import UserProfile from './components/UserProfile'
+import { useState, createContext } from "react";
+
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
+
+export const data = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
-  ["name=", "Alice", "age=", "25", "bio=", "Loves hiking and photography"]
-
   return (
     <>
-      <WelcomeMessage />  
-      <Footer />
+      <Counter />
+      <WelcomeMessage />
+
       <Header />
       <MainContent />
-      <UserProfile /> 
-
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
